2d26c7a8e9001bdce3bf6e7129a8a3ddd8488335
Dhananjay Narayan <dhananjayashwath@gmail.com>
2022-07-09 02:43:59 +0530

    Create moviekb.pl

---
+++ Assignment 1/moviekb.pl
@@ -0,0 +1,87 @@
+
+movie('tt7144666','The Black Phone',[horror,thriller],2021,7.3,[phone,murder,call,kidnapper,kids,ghost,survival,kidnap],[english],'https://www.imdb.com/title/tt7144666/mediaviewer/rm1575480577/?ref_=tt_ov_i,https://www.imdb.com/title/tt7144666/mediaviewer/rm2296847617/').
+movie('tt10709484','Don',[comedy,drama,romance],2022,7.0,[don,love,rowdy,sivakarthikeyan,ambition,goal,chechi],[tamil,telugu],'https://www.imdb.com/title/tt10709484/mediaviewer/rm3190024961/?ref_=tt_ov_i').
+movie('tt9851854','Major',[action,biography,drama],2022,8.6,[patriotism,sacrifice,war,martyr,love],[telugu,hindi],'https://www.imdb.com/title/tt9851854/mediaviewer/rm4257881345/?ref_=tt_ov_i').
+movie('tt8960382','Radhe Shyam',[romance,drama],2022,5.3,[love,destiny,prediction,future,premonition],[telugu,tamil,hindi],'https://www.imdb.com/title/tt8960382/mediaviewer/rm1017648897/?ref_=tt_ov_i').
+movie('tt2631186','Baahubali',[action,drama],2015,8.0,[kings,throne,fight,war,love,revenge,flashback],[telugu,hindi,tamil],'https://www.imdb.com/title/tt2631186/mediaviewer/rm1023504640/?ref_=tt_ov_i').
+movie('tt5440700','Theri',[action,crime,thriller],2016,7.3,[daughter,kidnap,love,death,revenge],[tamil,telugu],'https://www.imdb.com/title/tt5440700/mediaviewer/rm3047363585/?ref_=tt_ov_i').
+movie('tt1825683','Black Panther',[action,adventure,scifi],2018,7.3,[revenge,flashback,throne,king,war],[english,french,hindi,telugu,tamil],'https://www.imdb.com/title/tt1825683/mediaviewer/rm172972800/?ref_=tt_ov_i').
+movie('tt10545484','Evaru',[crime,mystery,thriller,drama],2019,8.2,[revenge,flashback,murder,spy,trauma,war],[telugu],'').
+movie('tt11301946','Beast',[action,comedy,thriller],2022,5.2,[hero,army,dance,hijack,mall],[tamil,telugu,hindi],'https://www.imdb.com/title/tt11301946/mediaviewer/rm545595137/?ref_=tt_ov_i').
+movie('tt1649303','Darling',[comedy,drama,romance],2010,7.3,[love,funny,dance,cheat,flashback],[telugu],'').
+movie('tt1922777','Sinister',[horror,thriller,mystery],2012,6.8,[ghost,father,haunted,family,exorcism],[english],'').
+movie('tt6148156','Vikram Vedha',[action,crime,drama,thriller],2017,8.2,[revenge,love,gangster,cop,murder,conspiracy],[tamil,telugu,hindi],'').
+movie('tt9263550','Rocketry',[biography,drama,action],2022,9.3,[truestory,sad,conspiracy,patriotism],[tamil,hindi],'https://www.imdb.com/title/tt9263550/mediaviewer/rm3143700737/?ref_=tt_ov_i').
+movie('tt8178634','RRR',[action,drama],2022,8.0,[patriotism,hero,brothers,dance,love],[telugu,tamil,hindi,kannada],'https://www.imdb.com/title/tt8178634/mediaviewer/rm1391397377/?ref_=tt_ov_i').
+movie('tt6836936','Saaho',[action,thriller],2019,5.0,[cop,thief,burglary,love,cheat],[telugu,hindi],'').
+movie('tt11374902','Doctor',[comedy,crime,thriller,mystery],2021,7.5,[kidnap,crime,love,baby,chellama],[tamil,telugu],'').
+movie('tt1191121','Jalsa',[action,comedy,romance],2008,7.4,[love,impress,dance,musical,breakup],[telugu],'https://www.imdb.com/title/tt1191121/mediaviewer/rm1790870273/?ref_=tt_ov_i').
+movie('tt5959980','Vada Chennai',[crime,action,thriller],2018,8.4,[crime,muder,thief,guns,war,violence],[tamil],'').
+movie('tt9900782','Kaithi',[crime,thriller],2019,8.5,[smuggling,drugs,war,biryani,multiverse,police,chase],[tamil],'').
+movie('tt3863552','Bajrangi Bhaijaan',[adventure,comedy,drama],2015,8.1,[chicken,pakistan,india,kid,child,reunion],[hindi],'').
+
+director('nm0220600','Scott Derrickson' ,male ,56).
+director('nm12268742','Cibi Chakravarthy' ,male ,33).
+director('nm3481620','Shashi Kiran Tikka' ,male ,38).
+director('nm1442514','Rajamouli' ,male ,49).
+director('nm1203138','Kabir Khan' ,male ,53).
+director('nm2379696','Radhakrishna Kumar' ,male ,33).
+director('nm2575525','Pushkar' ,male ,42).
+director('nm2566836','Gayatri' ,female,37).
+director('nm3363032','Ryan Coogler' ,male ,38).
+director('nm10829550','Venkat Ramji' ,male ,56).
+director('nm5961051','Atlee Kumar' ,male ,39).
+director('nm12135964','Nelson Dilipkumar' ,male,38).
+director('nm6682160','Sujeeth' ,male ,37).
+director('nm0534856','Madhavan' ,male ,52).
+director('nm0440269','Karunakaran' ,male ,48).
+director('nm7992231','Lokesh Kanagaraj' ,male ,36).
+director('nm4274575','Vetrimaaran' ,male ,47).
+
+actor('nm1659141' ,'Prabhas' ,male ,43).
+actor('nm5249562' ,'Pooja Hegde' ,female,32).
+actor('nm1912683' ,'Karthi' ,male ,45).
+actor('nm0000160' ,'Ethan Hawke' ,male ,52).
+actor('nm3414188' ,'Adivi Sesh' ,male ,37).
+actor('nm2281292' ,'Rana Daggubati' ,male ,38).
+actor('nm2011932' ,'Anushka Shetty' ,female,41).
+actor('nm1694524' ,'NTR' ,male ,38).
+actor('nm2776304' ,'Ram Charan' ,male ,37).
+actor('nm0006795' ,'Salman Khan' ,male ,57).
+actor('nm0534856' ,'Madhavan' ,male ,52).
+actor('nm0004626' ,'Kareena Kapoor' ,female,42).
+actor('nm0897201' ,'Joseph Vijay' ,male ,52).
+actor('nm3606487' ,'Samantha Ruth Prabhu' ,female,35).
+actor('nm3367112' ,'Amy Jackson' ,female,30).
+actor('nm6489058' ,'Yogi Babu' ,male ,45).
+actor('nm3601766' ,'Shraddha Kapoor' ,female,36).
+actor('nm1302330' ,'Murli Sharma' ,male ,53).
+actor('nm1017633' ,'Alia Bhatt' ,female,32).
+actor('nm2570245' ,'Kajal Aggarwal' ,female,37).
+actor('nm0436302' ,'Pawan Kalyan' ,male ,52).
+actor('nm9296746' ,'Mason Thames' ,male ,15).
+actor('nm6421259' ,'Madeleine McGraw' ,female,14).
+actor('nm10484501' ,'Priyanka Arulmohan' ,female,27). 
+actor('nm2284321' ,'Samuthirakani' ,male ,52).
+actor('nm4792434' ,'Sivakarthikeyan' ,male ,36).
+actor('nm0695177' ,'Prakash Raj' ,male ,59).
+actor('nm1569276' ,'Chadwick Boseman' ,male ,43).
+actor('nm1775091' ,'Danai Gurira' ,female,35).
+actor('nm0293509' ,'Martin Freeman' ,male ,51).
+actor('nm5294556' ,'Regina Cassandra' ,female,32).
+actor('nm0694893' ,'Prabhu' ,male ,65).
+actor('nm1628115' ,'Juliet Rylance' ,female,32).
+actor('nm0710447' ,'James Ransone' ,male ,36).
+actor('nm4043111' ,'Vijay Sethupathi' ,male ,50).
+actor('nm7744845' ,'Shraddha Srinath' ,female,30).
+actor('nm0451321' ,'Shah Rukh Khan' ,male ,58).
+actor('nm0801264' ,'Simran' ,female,45).
+actor('nm0103977' ,'Brahmanandam' ,male ,68).
+actor('nm1333687' ,'Dhanush' ,male ,49).
+actor('nm5781594' ,'Aishwarya Rajesh' ,female,30).
+actor('nm10183124' ,'Arjun Das' ,male ,28).
+actor('nm1230844' ,'Narain' ,male ,42).
+actor('nm7372970' ,'Harshaali Malhotra' ,female,14).
+
+
+
