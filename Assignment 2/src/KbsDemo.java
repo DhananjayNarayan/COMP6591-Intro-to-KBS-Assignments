073308d26396ef8e87ce3bd9ca8be10fb15dfa1f
Madhuvanthi Hemanathan <madhunarmi@gmail.com>
2022-07-24 22:50:17 -0400

    feat: fixes done

--- Assignment 2/src/KbsDemo.java
+++ Assignment 2/src/KbsDemo.java
@@ -43,17 +43,19 @@ public class KbsDemo {
             case "1":
                 findMovie();
                 showOptions();
+                return;
 
             case "2":
                 addMovie();
                 showOptions();
+                return;
 
             case "3":
                 System.out.println("Enter the movie id for which the title has to be changed:");
                 String movieID = s.nextLine();
                 updateMovie(movieID);
                 showOptions();
-
+                return;
             case "4":
                 System.out.println("Quitting from Knowledge Base");
                 return;
@@ -67,14 +69,18 @@ public class KbsDemo {
      */
     private void findMovie() {
         String title = getInput("Enter the movie title to search...");
-        String str2 = String.format("find_a_movie(\"%s\",?)", title);
+        String str2 = String.format("find_a_movie('%s',?)", title);
         Variable variables = new Variable("Movies");
         Query q2 = new Query(str2, variables);
-        System.out.println(str2 + " " + (q2.hasSolution() ? "Yes, the movie is present and below are its details" :
-                "No, the movie is not found"));
-        while (q2.hasNext()) {
-            System.out.println(q2.next());
+        if (q2.hasSolution() && q2.hasNext()) {
+            Term term = q2.next().get("Movies");
+            if (term.listToTermArray().length > 0) {
+                System.out.println(str2 + " Yes, the movie is present and below are its details");
+                System.out.println(term);
+                return;
+            }
         }
+        System.out.println(str2 + " No, the movie is not found");
     }
 
     /**
@@ -90,7 +96,7 @@ public class KbsDemo {
         movieDetails[4] = getInput("Enter the movie Rating");
         movieDetails[5] = getInput("Enter the movie keywords");
         movieDetails[6] = getInput("Enter the movie Language");
-        String str3 = String.format("add_movie('%s','%s','%s','%s','%s','%s','%s')",
+        String str3 = String.format("add_movie('%s','%s','%s','%s','%s',[%s],[%s])",
                 movieDetails[0], movieDetails[1], movieDetails[2], movieDetails[3],
                 movieDetails[4], movieDetails[5], movieDetails[6]);
         Query q3 = new Query(str3);
@@ -118,39 +124,45 @@ public class KbsDemo {
                 Query updQ1 = new Query(upd1);
                 System.out.println(upd1 + " " + (updQ1.hasSolution() ? "movie title is updated successfully" : "Couldn't update movie ID"));
                 updateMovie(movieID);
+                return;
             case "2":
                 String newGenre = getInput("Enter the movie's new genre:");
                 String upd2 = String.format("update_movie_genre('%s','%s')", movieID, newGenre);
                 Query updQ2 = new Query(upd2);
                 System.out.println(upd2 + " " + (updQ2.hasSolution() ? "movie genre is updated successfully" : "Couldn't update movie genre"));
                 updateMovie(movieID);
+                return;
             case "3":
                 String newRating = getInput("Enter the movie's new rating:");
                 String upd3 = String.format("update_movie_rating('%s','%s')", movieID, newRating);
                 Query updQ3 = new Query(upd3);
                 System.out.println(upd3 + " " + (updQ3.hasSolution() ? "movie rating is updated successfully" : "Couldn't update movie rating"));
                 updateMovie(movieID);
+                return;
             case "4":
                 String newYear = getInput("Enter the movie's new year:");
                 String upd4 = String.format("update_movie_year('%s','%s')", movieID, newYear);
                 Query updQ4 = new Query(upd4);
                 System.out.println(upd4 + " " + (updQ4.hasSolution() ? "movie year is updated successfully" : "Couldn't update movie year"));
                 updateMovie(movieID);
+                return;
             case "5":
                 String newKeywords = getInput("Enter the movie's new keywords as comma separated values:");
-                String upd5 = String.format("update_movie_keywords('%s','%s')", movieID, newKeywords);
+                String upd5 = String.format("update_movie_keywords('%s',[%s])", movieID, newKeywords);
                 Query updQ5 = new Query(upd5);
                 System.out.println(upd5 + " " + (updQ5.hasSolution() ? "movie keywords are updated successfully" : "Couldn't update movie keywords"));
                 updateMovie(movieID);
+                return;
             case "6":
                 String newLang = getInput("Enter the movie's new language:");
-                String upd6 = String.format("update_movie_rating('%s','%s')", movieID, newLang);
+                String upd6 = String.format("update_movie_languages('%s',[%s])", movieID, newLang);
                 Query updQ6 = new Query(upd6);
                 System.out.println(upd6 + " " + (updQ6.hasSolution() ? "movie language is updated successfully" : "Couldn't update movie language"));
                 updateMovie(movieID);
+                return;
             case "7":
                 System.out.println("Exiting from update function");
-                updateMovie(movieID);
+                return;
         }
         System.out.println("Select valid options from the list");
         updateMovie(movieID);
